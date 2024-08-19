361556cb52ff9539132e492b2d8ddb64db71e1d8
amer2112 <151033635+amer2112@users.noreply.github.com>
2024-08-19 14:07:32 +0300

    Delete save_email.php

--- save_email.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $email = $_POST['email'];
-    file_put_contents('user_emails.txt', $email . PHP_EOL, FILE_APPEND);
-    echo 'Email saved';
-}
-?>
