d5dbe291efa55462ecd54f37e7007b2800020390
amer2112 <151033635+amer2112@users.noreply.github.com>
2024-08-19 15:31:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -54,33 +54,6 @@ function loadContent(contentType) {
             <button id="calculate-button" onclick="calculateCalories()">Calculate</button>
             <p id="calorie-result"></p>
         `;
-    } else if (contentType === 'my-story') {
-        contentArea.innerHTML = `
-            <ul>
-                <li onclick="loadContent('certification')">Certification</li>
-                <li onclick="loadContent('videos')">Videos</li>
-                <li onclick="loadContent('why-all-this')">Why All This?</li>
-            </ul>`;
-    } else if (contentType === 'current-offers') {
-        contentArea.innerHTML = `
-            <ul>
-                <li onclick="selectCurrency('limited')">Limited Time Offers</li>
-                <li onclick="selectCurrency('standard')">Standard Offers</li>
-            </ul>`;
-    } else if (contentType === 'exercise-library') {
-        contentArea.innerHTML = `
-            <ul>
-                <li onclick="loadExerciseVideo('back-exercises')">Back Exercises</li>
-                <li onclick="loadExerciseVideo('chest-exercises')">Chest Exercises</li>
-                <li onclick="loadExerciseVideo('shoulder-exercises')">Shoulder Exercises</li>
-                <li onclick="loadExerciseVideo('bicep-exercises')">Bicep Exercises</li>
-                <li onclick="loadExerciseVideo('tricep-exercises')">Tricep Exercises</li>
-                <li onclick="loadExerciseVideo('legs-exercises')">Legs Exercises</li>
-                <li onclick="loadExerciseVideo('abs-exercises')">Abs Exercises</li>
-                <li onclick="loadExerciseVideo('dynamic-stretching')">Dynamic Stretching</li>
-                <li onclick="loadExerciseVideo('mobility-exercises')">Mobility Exercises</li>
-                <li onclick="loadExerciseVideo('cardiovascular-exercises')">Cardiovascular Exercises</li>
-            </ul>`;
     }
 }
 
@@ -123,13 +96,9 @@ function loadOffers(currency) {
                            '- Full package (training and meal plan) 1 month: 1200 EGP\n' +
                            '- Full package 3 months: 3300 EGP';
         } else if (currency === 'USD') {
+            // Insert the offers for USD here
             offerContent = `Standard Offers (${currency}):\n` +
-                           '- Meal plan 1 month: 20$\n' +
-                           '- Meal plan 3 months: 50$\n\n' +
-                           '- Training plan 1 month: 40$\n' +
-                           '- Training plan 3 months: 100$\n\n' +
-                           '- Full package (training and meal plan) 1 month: 50$\n' +
-                           '- Full package 3 months: 130$';
+                           '/* Insert your USD offers here */';
         }
     }
 
